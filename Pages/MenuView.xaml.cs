d0362d9942626a347c3f5213dd970eca37f7d4f4
Tomasz Jakubiak <tomasz.jakubiak9@gmail.com>
2021-05-13 23:18:46 +0200

    Initcommit
    Create and configure project

---
+++ Pages/MenuView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Forms.Views;
+using NewsApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileNewsApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuView : MvxContentPage<AllNewsViewModel>
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
